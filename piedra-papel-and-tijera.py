9e9f207ac4061b1e01d3a6f5634fa0388d5fc050
zeroforte <147382689+xzerofortex@users.noreply.github.com>
2024-10-07 00:57:25 -0700

    Update and rename Código.py to piedra-papel-and-tijera.py

---
+++ piedra-papel-and-tijera.py
@@ -0,0 +1,81 @@
+
+# piedra papel and tijera
+import random
+import time
+
+elements = ('piedra','papel','tijera')
+intentos = 0
+
+# mantiene la partida en juego asta que gane el usuario
+while True:
+    bot =random.choice(elements)
+    you =input('elije (priedra/papel/tijera):  ')
+
+    # verifica si el usuario puso (piedra/papel/tijera) sino se repite
+    if  you not in (elements):
+        print('elemento no valido, usa uno de los siguientes (piedra/papel/tijera): ')
+        continue
+
+    #se acumulan los intentos
+    intentos += 1
+    if you == 'piedra' and bot == 'piedra':
+        print(f'empate  tu:({you})  -  bot:({bot})')
+        time.sleep(1)
+    elif you == 'papel' and bot == 'papel':
+        print(f'empate  tu:({you})  -  bot:({bot})')
+        time.sleep(1)
+    elif you == 'tijera' and bot == 'tijera':
+        print(f'empate  tu:({you})  -  bot:({bot})')
+        time.sleep(1)
+    elif you == 'piedra' and bot == 'papel':
+        print(f'perdistes  tu:({you})  -  bot:({bot})')
+        time.sleep(1)
+    elif you == 'papel' and bot == 'tijera':
+        print(f'perdistes  tu:({you})  -  bot:({bot})')
+        time.sleep(1)
+    elif you == 'tijera' and bot == 'piedra':
+        print(f'perdistes  tu:({you})  -  bot:({bot})')
+        time.sleep(1)
+    elif you == 'piedra' and bot == 'tijera':
+        print(f'ganastes  tu:({you})  -  bot:({bot})')
+        time.sleep(1)
+        break
+    elif you == 'papel' and bot == 'piedra':
+        print(f'ganastes  tu:({you})  -  bot:({bot})')
+        time.sleep(1)
+        break
+    elif you == 'tijera' and bot == 'papel':
+        print(f'perdistes  tu:({you})  -  bot:({bot})')
+        time.sleep(1)
+        break
+
+
+print(f'el juego termino con intentos:({intentos})')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
